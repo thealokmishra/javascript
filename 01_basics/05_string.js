d5853b6460ee79055b12cc57b5ae9946fb00c01e
Alok Mishra <91321828+thealokmishra@users.noreply.github.com>
2026-01-22 18:24:26 +0000

    strings intro

---
+++ 01_basics/05_string.js
@@ -0,0 +1,17 @@
+const name = "Alok";
+const lName = "Mishra";
+
+//using backticks ``, for concatenation
+const greeting = `Hello my name is ${name} ${lName}`
+console.log(greeting); //out: Hello my name is Alok Mishra
+
+//string is stored as an object with key : value pairs, keys are like index which hold the corresponding value of the character at that place
+
+//console.log(name[0]); // out: 'A' , because at 0th place A is present subsequently "l" "o" and "k"
+
+// accessing string methods
+console.log(name.length);    //out: 4
+console.log(name.toUpperCase()); //out: ALOK
+
+
+
