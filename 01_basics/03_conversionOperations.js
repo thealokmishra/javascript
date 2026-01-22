2e7a57dc4344c94e871c5011d537d9a77b57039f
Alok Mishra <91321828+thealokmishra@users.noreply.github.com>
2026-01-22 13:22:17 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)  //convert to number
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)  //output: NaN = not a number, because score contains alphanumeric string can't convert, 
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)  //output: NaN = not a number, because score contains alphanumeric string can't convert, 
 
 // conversion to Number for different types of value in score:
 // "33" => 33
@@ -27,3 +27,22 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 
 // 33 => "33"
+
+
+// ***************OPERATIONS**************
+
+// 2**3 two raised to power three
+// 2%3 remainder
+// 2/3 divide
+
+// if string is first then everything will be converted to string
+// console.log("1" + 2 + 2);  out: 122
+
+// if numbers are first then operations will be performed then concatenation
+// console.log(1 + 2 + "2");  out: 32
+
+let counter = 100
+// ++counter
+// counter++
+// what happens when we prefix operator vs postfix operator
+// console.log(counter)
