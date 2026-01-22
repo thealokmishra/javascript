650a7c30c9565e1ed002f1d11a65afece27062b3
Alok Mishra <91321828+thealokmishra@users.noreply.github.com>
2026-01-22 12:02:35 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version js
+
+//  alert(3+3) //can't be used in nodejs, only browser
+
+let name = "Alok"    //string
+let age = 3          //int
+let isLogged = false  //bool
+
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+
+// learn more about null vs undefined (IMP)
+
+
+// object
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)  //object
