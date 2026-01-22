63efa271e3a657817406c21d4ae9dc73415846f9
Alok Mishra <91321828+thealokmishra@users.noreply.github.com>
2026-01-22 16:22:58 +0000

    stack & heap memory in js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,6 @@
-//  Primitive
+// js is a dynamically typed language means var types are defined at runtime & we don't need to explicitly declare the type of var like if it's Number, string or bool.
+
+//  Primitive - you get a copy
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
@@ -19,7 +21,7 @@ console.log(id === anotherId);
 
 
 
-// Reference type (Non primitive)
+// Reference type (Non primitive) - you get a reference
 
 // Array, Objects, Functions
 
@@ -38,3 +40,24 @@ const myFunction = function(){
 console.log(typeof anotherId);  //check typeof data
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// *************************
+
+// Stack(Primitive) - copy is created 
+
+let myName = "Alok";   //declare
+let newName =  myName;  //creates a copy from myName
+
+// Heap(Non-Primitive) - reference is given
+let userOne = {
+    email: "some@gmail.com",
+    name: "Alok"
+}
+
+let userTwo = userOne //reference user2 from user
+userTwo.email = "another@gmail.com"
+
+// we only changed email attribute in userTwo but it was chnaged for both one & two, because they are referencing to the same place unlike primitive types
+console.log(userOne.email)  //out: another@gmail.com
+console.log(userTwo.email)  //out: another@gmail.com
+
