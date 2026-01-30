6baed41e8d58117e268db45e06c3a9ea40267609
Alok Mishra <91321828+thealokmishra@users.noreply.github.com>
2026-01-30 18:59:13 +0530

    iife functions

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IIFE): used to run functions immediately
+//also used to remove global scope pollution
+
+(function chai() {
+  // named IIFE
+  console.log(`DB CONNECTED`);
+})();
+
+((name) => {
+  console.log(`DB CONNECTED TWO ${name}`);
+})("hitesh")((params) => {
+  console.log(`DB CONNECTED TWO ${params}`);
+})("argument");
+
+//fn is declared in first paranthesis, then called in second parenthesis
